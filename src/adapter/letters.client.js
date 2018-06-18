8be385805656436c177cb9c33d04b48acfaa38c6
Szymon Stanisz <szymon.stanisz@thesoftwarehouse.pl>
2018-06-18 17:44:12 +0200

    logical operator correction

--- src/adapter/letters.client.js
+++ src/adapter/letters.client.js
@@ -17,15 +17,17 @@ class LettersClient {
                 seatA === 'F' && seatB !== 'A' ||
                 seatA === 'B' && seatB !== 'A' && seatB !== 'F' && seatB !== 'E' ||
                 seatA === 'E' && seatB !== 'A' && seatB !== 'F' && seatB !== 'B'
-        ) {
+            ) {
                 return -1;
             } else if (
+                seatB === 'A' ||
                 seatB === 'A' && seatA !== 'F' ||
                 seatB === 'F' && seatA !== 'A' ||
                 seatB === 'B' && seatA !== 'A' && seatA !== 'F' && seatA !== 'E' ||
                 seatB === 'E' && seatA !== 'A' && seatA !== 'F' && seatA !== 'B' ||
                 seatB === 'D' && seatA !== 'A' && seatA !== 'F' && seatA !== 'E' && seatA == 'B' ||
                 seatB === 'C' && seatA !== 'A' && seatA !== 'F' && seatA !== 'E' && seatA == 'B'
+
             ) {
                 return 1;
             }
